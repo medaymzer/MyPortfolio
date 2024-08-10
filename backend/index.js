40624ba460b1308bf7a014fda7ea0b32733bebd4
medaymzer <zermanehamza919@gmail.com>
2024-08-10 20:09:50 +0200

    update

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const projectsRoutes = require('./routes/projects')
+const cors = require('cors')
+//express app
+const app = express()
+//midlleware
+app.use(cors({
+    origin: 'https://my-portfolio-zermedaymen.vercel.app',
+  }));
+app.use(express.json())
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
+//routes
+app.use('/api/projects',projectsRoutes)
+
+//connect to db 
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("good")
+})
+.catch((error)=>{
+    console.log(error)
+})
+
+//listen for requests
+
+app.listen(process.env.PORT,() => {
+    console.log("listening on port 4000")
+})
