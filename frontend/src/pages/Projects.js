010a6e43f8250db0555aecbac4f03ba5caa7d000
medaymzer <zermanehamza919@gmail.com>
2024-08-12 17:59:45 +0200

    Update Projects.js

--- frontend/src/pages/Projects.js
+++ frontend/src/pages/Projects.js
@@ -11,7 +11,47 @@ const Projects = () => {
     const [projects,setProjects] = useState(null)
     const [loading, setLoading] = useState(true);
 
-    useEffect(()=>{
+    const projs = [
+        { id: 1, title: 'Tecsas', type: "Développement complet du site", date: 'avril 2020',description : "Description du projet. Présentez une vue d'ensemble ou donnez des détails sur ce qui vous a inspiré, comment vous l'avez créé, ou tout autre élément que vous souhaitez partager avec les visiteurs. Pour ajouter des descriptions de projet, allez à Gérer les projets.",screenshotBase64 : ["https://raw.githubusercontent.com/medaymzer/testvercel/main/tecsasthumb-min.png","https://raw.githubusercontent.com/medaymzer/testvercel/main/FireShot%20Pro%20Webpage%20Capture%20006%20-%20'Preview'%20-%20editor.wix.com-min.png"] },
+        { id: 2, title: 'Heliotech', type: "Développement front end du site", date: 'mai 2021',description : "Description du projet. Présentez une vue d'ensemble ou donnez des détails sur ce qui vous a inspiré, comment vous l'avez créé, ou tout autre élément que vous souhaitez partager avec les visiteurs. Pour ajouter des descriptions de projet, allez à Gérer les projets.",screenshotBase64 : ["https://raw.githubusercontent.com/medaymzer/testvercel/main/heliotech%20thumb-min.png","https://raw.githubusercontent.com/medaymzer/testvercel/main/FireShot%20Pro%20Webpage%20Capture%20004%20-%20'Preview'%20-%20editor.wix.com-min.png"] },
+        { id: 3, title: 'Archidoc', type: "conception et développement du site", date: 'juin 2020',description : "Description du projet. Présentez une vue d'ensemble ou donnez des détails sur ce qui vous a inspiré, comment vous l'avez créé, ou tout autre élément que vous souhaitez partager avec les visiteurs. Pour ajouter des descriptions de projet, allez à Gérer les projets.",screenshotBase64 : ["https://raw.githubusercontent.com/medaymzer/testvercel/main/archidoc%20thumb-min.png","https://raw.githubusercontent.com/medaymzer/testvercel/main/FireShot%20Pro%20Webpage%20Capture%20003%20-%20'Preview'%20-%20editor.wix.com-min.png"] },
+        { id: 4, title: 'HK&C', type: "Développement front end", date: 'juillet 2022',description : "Description du projet. Présentez une vue d'ensemble ou donnez des détails sur ce qui vous a inspiré, comment vous l'avez créé, ou tout autre élément que vous souhaitez partager avec les visiteurs. Pour ajouter des descriptions de projet, allez à Gérer les projets.",screenshotBase64 : ["https://raw.githubusercontent.com/medaymzer/testvercel/main/hkc%20thumb-min.png","https://raw.githubusercontent.com/medaymzer/testvercel/main/FireShot%20Pro%20Webpage%20Capture%20005%20-%20'Preview'%20-%20editor.wix.com-min.png"] }
+        
+      ];
+
+      useEffect(()=>{
+        localStorage.clear()
+        const storedData = localStorage.getItem('exampleData');
+
+        if (storedData) {
+            setProjects(JSON.parse(storedData));
+            setLoading(false);
+        }else{
+
+
+        const fetchProjects = async () =>{
+            try{
+            const response = projs
+            console.log(response)
+           // const json = await response.json()
+            if (response){
+
+                setProjects(response)
+                localStorage.setItem('exampleData', JSON.stringify(response));
+                console.log("okok")
+            }
+            }catch(error){
+                console.log(error)
+            }finally{
+                setLoading(false)
+            }
+        }
+
+        fetchProjects()
+    }}, [])
+    
+        
+   /* useEffect(()=>{
         localStorage.clear()
         const storedData = localStorage.getItem('exampleData');
 
@@ -24,18 +64,14 @@ const Projects = () => {
         const fetchProjects = async () =>{
             try{
             const response = await fetch('/api/projects')
-                
             const json = await response.json()
-                console.log(response.json())
-                console.log(json)
             if (response.ok){
-                console.log("okokokok")
                 setProjects(json)
                 localStorage.setItem('exampleData', JSON.stringify(json));
                 console.log("okok")
             }
             }catch(error){
-                console.log(error.message)
+                console.log(error)
             }finally{
                 setLoading(false)
             }
@@ -43,7 +79,7 @@ const Projects = () => {
 
         fetchProjects()
     }}, [])
-
+*/
     if(loading){
         return(
             <div className='loading__'>
@@ -67,7 +103,7 @@ const Projects = () => {
             <div className='portfolio'>
             {projects && projects.map((project) =>(
                // <div></div>
-           <Project key={project._id} project={project}/>
+           <Project key={project.id} project={project}/>
             ))}
             </div>
 
